4f0a592f5ae44108270c68e719fb11ba20c721ad
SinaKhe <sinakh344@gmail.com>
2022-07-20 05:08:40 -0600

    images & fonts & colors & strings

---
+++ lib/models/fake_data.dart
@@ -0,0 +1,115 @@
+import 'package:tech_blog_app_3/gen/assets.gen.dart';
+
+import 'data_models.dart';
+
+Map homePagePosterMap = {
+  "imageAsset": Assets.images.poster.path,
+  "writer": "نویسنده",
+  "date": "یک روز پیش",
+  "title": "دوازده قدم برنامه نویسی یک دوره ",
+  "view": "251",
+};
+
+// blog fake data
+List<HashTagModel> tagList = [
+  HashTagModel(title: "جاوا"),
+  HashTagModel(title: "کاتلین"),
+  HashTagModel(title: "برنامه نویسی وب"),
+  HashTagModel(title: "هوش مصنوعی"),
+  HashTagModel(title: "iot"),
+  HashTagModel(title: "دارت"),
+  HashTagModel(title: "پادکست رادیو کدیاد"),
+];
+
+List<BlogModel> blogList = [
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+  BlogModel(
+    id: 1,
+    imageUrl: "",
+    title: "",
+    writer: "",
+    writerImageUrl: "",
+    date: "",
+    content: "",
+    views: "",
+  ),
+];
